eb2e0e650ecdf275efc0b7dad309b638dbfe3818
Harsh Thakur <harshprojects2002@outlook.com>
2024-09-11 02:00:12 +0530

    Changes : For Backend deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,8 +12,27 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Enables CORS support 
+const allowedOrigins = [
+  'http://localhost:3000', // Localhost
+  'https://password-manager-coral-two.vercel.app/', // Replace with your Vercel domain
+];
+
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+// Enable CORS
+app.use(cors(corsOptions));
+
 // Middlwears //
-app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
